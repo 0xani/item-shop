94ac67cf3143b12c743c32eedffbee727b98e08d
Jose Torres <jtorresuci@gmail.com>
2020-08-02 23:38:29 -0700

    Updates

---
+++ precache-manifest.e9f597f5b687fd3a12fcfc235cda3c83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb568f799243df4df13a55fab19fe53",
+    "url": "/item-shop/index.html"
+  },
+  {
+    "revision": "2f0763f41cb315e24607",
+    "url": "/item-shop/static/css/main.22f094c4.chunk.css"
+  },
+  {
+    "revision": "366daa5b4e8338817003",
+    "url": "/item-shop/static/js/2.2a5383bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/item-shop/static/js/2.2a5383bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0763f41cb315e24607",
+    "url": "/item-shop/static/js/main.4237c383.chunk.js"
+  },
+  {
+    "revision": "6e4709aa18a30407be5d",
+    "url": "/item-shop/static/js/runtime-main.bbf14afc.js"
+  },
+  {
+    "revision": "3bd7e5fa3f013c4da2f5dfd9e0e0676b",
+    "url": "/item-shop/static/media/Burbank.3bd7e5fa.ttf"
+  }
+]);
