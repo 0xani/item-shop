4ea98f98333a9d7a0be7b17c68a4baec1827de69
Jose Torres <jtorresuci@gmail.com>
2020-08-02 23:38:00 -0700

    Updates

---
+++ precache-manifest.d524993c995da9010fae1a8f917729f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c661909680b2dfc9ed3534a2804f6785",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "b8c5686ba95948bec1fd",
+    "url": "/portfolio-site/static/css/main.7f9423d9.chunk.css"
+  },
+  {
+    "revision": "366daa5b4e8338817003",
+    "url": "/portfolio-site/static/js/2.2a5383bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-site/static/js/2.2a5383bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c5686ba95948bec1fd",
+    "url": "/portfolio-site/static/js/main.1c059bda.chunk.js"
+  },
+  {
+    "revision": "3cfa024e5061e268a6b9",
+    "url": "/portfolio-site/static/js/runtime-main.9a0d7183.js"
+  },
+  {
+    "revision": "3bd7e5fa3f013c4da2f5dfd9e0e0676b",
+    "url": "/portfolio-site/static/media/Burbank.3bd7e5fa.ttf"
+  }
+]);
